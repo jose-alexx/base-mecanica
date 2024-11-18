3087dc8213bacb3c849ecce7c480b64d8f8259ca
jose alex <josealex41012@gmail.com>
2024-11-18 08:50:46 -0300

    testando estradas do esp32

--- testando-led/testando-led.ino
+++ testando-led/testando-led.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+// Definição dos pinos
+const int led1Pin = 19;
+const int led2Pin = 33;
+const int led3Pin = 32;
+const int buzzerPin = 25;
 
+void setup() {
+  // Definindo os pinos como saída
+  pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
+  pinMode(led3Pin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Acende o LED1 e ativa o buzzer
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
+  delay(500);  // Espera 500 ms
 
+  // Desliga o LED1 e desativa o buzzer
+  digitalWrite(led1Pin, LOW);
+  digitalWrite(buzzerPin, LOW);
+  delay(500);  // Espera 500 ms
+
+  // Acende o LED2 e ativa o buzzer
+  digitalWrite(led2Pin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
+  delay(500);  // Espera 500 ms
+
+  // Desliga o LED2 e desativa o buzzer
+  digitalWrite(led2Pin, LOW);
+  digitalWrite(buzzerPin, LOW);
+  delay(500);  // Espera 500 ms
+
+  // Acende o LED3 e ativa o buzzer
+  digitalWrite(led3Pin, HIGH);
+  digitalWrite(buzzerPin, HIGH);
+  delay(500);  // Espera 500 ms
+
+  // Desliga o LED3 e desativa o buzzer
+  digitalWrite(led3Pin, LOW);
+  digitalWrite(buzzerPin, LOW);
+  delay(500);  // Espera 500 ms
 }
+
